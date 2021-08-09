7050daae71ab1bdf16d583dad26fbf8c71cec65c
Fernando Arciga <54166773+ghero56@users.noreply.github.com>
2021-08-09 00:03:18 -0500

    Add files via upload

---
+++ Server/server.py
@@ -0,0 +1,18 @@
+import os
+
+os.system('clear')
+obj = os.uname()
+print("***********************",obj.sysname,"************************")
+print("*********",obj.release,"**********")
+print("**********************",obj.machine,"************************")
+print("********",obj.version,"*********")
+print("*****Estableciendo conexión desde 192.186.100.243*****")
+print("*************Iniciando sesión: 315285705**************")
+print("************Acceso concedido para: Fernando***********")
+print("\n")
+print("*****************Solicitud de: 'Areas'****************")
+print("\n")
+print("****************En espera de Solicitud****************")
+print("\\")
+print("\n")
+print("\n")
